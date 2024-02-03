d71be74351ce549cb21e8023fe57de2e870100e5
Omer Bircan Sahin <omerbsahin@gmail.com>
2024-02-03 09:48:17 +0300

    desktop completed

--- src/components/AgeCalculator.js
+++ src/components/AgeCalculator.js
@@ -1,6 +1,6 @@
 import './style/global.css'
 import iconArrow from './static-images/icon-arrow.svg'
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function AgeCalculator() {
 
@@ -13,64 +13,120 @@ export default function AgeCalculator() {
     const [errorCodeOfYear, setErrorCodeOfYear] = useState("")
 
     const [isThereError, setIsThereError] = useState(false)
+
     const [borderColor, setBorderColor] = useState("1px solid rgba(71, 85, 105, 0.25)")
     const [labelColor, setLabelColor] = useState("hsl(0, 1%, 44%)")
 
-    const hasPageRendered = useRef(false)
+    const [yearDigits, setYearDigits] = useState("- -")
+    const [monthDigits, setMonthDigits] = useState("- -")
+    const [dayDigits, setDayDigits] = useState("- -")
+
 
     const validateDate = require('validate-date')
+
     useEffect(() => {
 
-        if (hasPageRendered.current) {
-            if (isThereError) {
-                setBorderColor("1px solid #dc2626")
-                setLabelColor("#dc2626")
-            }
-            //no error condition
-            else {
-                setBorderColor("1px solid rgba(71, 85, 105, 0.25)")
-                setLabelColor("hsl(0, 1%, 44%)")
-                validateDate(`${monthInput}/${dayInput}/${yearInput}`)
-            }
+        if (isThereError) {
+            setBorderColor("1px solid #dc2626")
+            setLabelColor("#dc2626")
         }
-        hasPageRendered.current = true
-        // eslint-disable-next-line
+        //no error condition
+        else {
+            setBorderColor("1px solid rgba(71, 85, 105, 0.25)")
+            setLabelColor("hsl(0, 1%, 44%)")
+        }
+
+
     }, [isThereError]);
 
     const submitCheck = (e) => {
 
+        let error = 0
+
         if (dayInput.length === 0) {
             setErrorCodeOfDay("This field is required")
             setIsThereError(true)
+            error = 0
         } else if (dayInput > 31) {
             setErrorCodeOfDay("Must be a valid day")
             setIsThereError(true)
+            error = 0
         } else {
             setErrorCodeOfDay("")
-            setIsThereError(false)
+            error++
         }
 
         if (monthInput.length === 0) {
             setErrorCodeOfMonth("This field is required")
             setIsThereError(true)
+            error = 0
+
         } else if (monthInput > 12) {
             setErrorCodeOfMonth("Must be a valid month")
             setIsThereError(true)
+            error = 0
+
         } else {
             setErrorCodeOfMonth("")
-            setIsThereError(false)
+            error++
         }
 
 
         if (yearInput.length === 0) {
             setErrorCodeOfYear("This field is required")
             setIsThereError(true)
+            error = 0
+
         } else if (yearInput > 2024) {
             setErrorCodeOfYear("Must be in the past")
             setIsThereError(true)
+            error = 0
+
         } else {
             setErrorCodeOfYear("")
-            setIsThereError(false)
+            error++
+
+        }
+
+
+        if (error === 3) {
+            let day = 0
+            let month = 0
+
+            if (dayInput.length === 1) {
+                setDayInput(`0${dayInput}`)
+                day = `0${dayInput}`
+            } else day = dayInput
+            if (monthInput.length === 1) {
+                setMonthInput(`0${monthInput}`)
+                month = `0${monthInput}`
+            } else month = monthInput
+
+            if (validateDate(`${month}/${day}/${yearInput}`) === "Invalid Date") setErrorCodeOfDay("Must be a valid date")
+            //no error condition
+            else {
+
+                let yearCal = 0
+                let monthCal = 0
+                let dayCal
+
+                setIsThereError(false)
+                let dateDifferenceAsDay = Math.abs(new Date(parseInt(yearInput), parseInt(month) - 1, parseInt(day)) - new Date()) / 86400000;
+
+                while (dateDifferenceAsDay >= 365.25) {
+                    dateDifferenceAsDay -= 365.25
+                    yearCal++
+                }
+                while (dateDifferenceAsDay >= 30) {
+                    dateDifferenceAsDay -= 30
+                    monthCal++
+                }
+                dayCal = Math.floor(dateDifferenceAsDay)
+
+                setYearDigits(yearCal.toString())
+                setMonthDigits(monthCal.toString())
+                setDayDigits(dayCal.toString())
+            }
         }
 
     }
@@ -98,7 +154,7 @@ export default function AgeCalculator() {
             {/*Main Structure*/}
             <div
                 style={{
-                    borderRadius: "1.5rem 1.5rem 200px 1.5rem"
+                    borderRadius: "1.5rem 1.5rem 170px 1.5rem"
                 }}
                 className={"flex flex-col gap-y-1 bg-white p-12 rounded-tl-3xl rounded-tr-3xl rounded-bl-3xl"}>
                 {/*Upper input section*/}
@@ -155,25 +211,22 @@ export default function AgeCalculator() {
                 {/*Middle button section*/}
                 <div className={"flex items-center"}>
                     <hr style={{minWidth: "450px"}} className={"grow"}/>
-                    <button onClick={submitCheck} id={"clicker"} className={"rounded-full p-2"}>
-                        <img alt={"btn"} src={iconArrow}/>
+                    <button type={"submit"} onClick={submitCheck} id={"clicker"} className={"rounded-full p-4"}>
+                        <img className={"w-8"} alt={"btn"} src={iconArrow}/>
                     </button>
                 </div>
                 {/*Below age section*/}
                 <div className={"flex flex-col gap-y-3"}>
                     <div className={"flex gap-x-3 text-6xl font-extrabold italic"}>
-                        <div style={{color: "hsl(259, 100%, 65%)"}}>-</div>
-                        <div style={{color: "hsl(259, 100%, 65%)"}}>-</div>
+                        <div style={{color: "hsl(259, 100%, 65%)"}}>{yearDigits}</div>
                         <div>years</div>
                     </div>
                     <div className={"flex gap-x-3 text-6xl font-extrabold italic"}>
-                        <div style={{color: "hsl(259, 100%, 65%)"}}>-</div>
-                        <div style={{color: "hsl(259, 100%, 65%)"}}>-</div>
+                        <div style={{color: "hsl(259, 100%, 65%)"}}>{monthDigits}</div>
                         <div>months</div>
                     </div>
                     <div className={"flex gap-x-3 text-6xl font-extrabold italic"}>
-                        <div style={{color: "hsl(259, 100%, 65%)"}}>-</div>
-                        <div style={{color: "hsl(259, 100%, 65%)"}}>-</div>
+                        <div style={{color: "hsl(259, 100%, 65%)"}}>{dayDigits}</div>
                         <div>days</div>
                     </div>
                 </div>
